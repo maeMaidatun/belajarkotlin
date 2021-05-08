c7e6c64a31e7480e8ca840c009180ae12a15195b
Maidatun  Izzati <maidatun1224@gmail.com>
2021-05-08 01:03:22 -0500

    belajarkotlin

---
+++ app/src/main/java/com/ilhamfidatama/androidwithkotlin/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.ilhamfidatama.androidwithkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.frame1, MovieFragment())
+            .commit()
+    }
+}
